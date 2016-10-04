e47411b016590e9f4ea3679f965271cad12f90ff
Endel Dreyer <endel.dreyer@gmail.com>
2016-10-04 22:17:00 +0200

    add Benchmarks solution

---
+++ Benchmarks/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace Benchmarks
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var summary = BenchmarkRunner.Run<FossilDelta>();
+			Console.WriteLine (summary.ToString());
+		}
+	}
+}
