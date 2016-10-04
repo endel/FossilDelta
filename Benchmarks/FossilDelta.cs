c3d6e0f1d0d964bb7d8010587a7a9e9430eed3c3
Endel Dreyer <endel.dreyer@gmail.com>
2016-10-04 22:17:00 +0200

    add Benchmarks solution

---
+++ Benchmarks/FossilDelta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace Benchmarks
+{
+	public class FossilDelta
+	{
+
+		[Benchmark]
+		public byte[] CreateDelta()
+		{
+			return Fossil.Delta.Create(Samples.origin1, Samples.target1);
+		}
+	}
+}
+
